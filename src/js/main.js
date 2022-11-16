e7a01e33dce4c27a731b7e8a4dd7688e303a16b8
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-11-16 21:04:23 +0000

    add basic repro set up and set up kaboom.js

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+    
+kaboom({
+    width: 700,
+    height: 500,
+    font: "sinko",
+    canvas: document.querySelector("#mycanvas"),
+    background: [ 15, 33, 12, ],
+})
